3f745e62ee263a78d3bc8893174e3fbaff4ee8b0
emma <du.changkun@cloud-ace.com>
2024-07-15 23:38:36 +0800

    Revert "add bge-m3 vertex ai model"
    This reverts commit e38ad9ec3ab7d75fc40fcd920f11bf02eed891ad.

--- mteb/models/bge_models.py
+++ mteb/models/bge_models.py
@@ -1,72 +1,60 @@
 from __future__ import annotations
 
-import logging
 from functools import partial
 from typing import Any
 
-import numpy as np
+import torch
+from sentence_transformers import SentenceTransformer
 
 from mteb.model_meta import ModelMeta
 from mteb.models.text_formatting_utils import corpus_to_texts
-from mteb.requires_package import requires_package
 
-logger = logging.getLogger(__name__)
 
 class BGEWrapper:
-    def __init__(self, model_name: str, embed_dim: int | None = None, **kwargs) -> None:
-        from google.cloud import aiplatform
-        self._client = aiplatform.Endpoint("366038416002908160")
-        self._model_name = model_name
-        self._embed_dim = embed_dim
+    """following the hf model card documentation."""
 
-    def encode(
+    def __init__(self, model_name: str, **kwargs: Any):
+        self.model_name = model_name
+        self.mdl = SentenceTransformer(model_name)
+
+    def to(self, device: torch.device) -> None:
+        self.mdl.to(device)
+
+    def encode(  # type: ignore
         self,
         sentences: list[str],
         *,
-        batch_size: int = 250,
-        prompt_name: str | None = None,
+        batch_size: int = 32,
         **kwargs: Any,
-    ) -> np.ndarray:
-        embeddings = []
-        for batch in range(0, len(sentences), batch_size):
-            text_batch = sentences[batch : batch + batch_size]
-            instances = [[text] for text in text_batch]
-            prediction = self._client.predict(instances=instances)
-            embeddings.extend(prediction.predictions[0][0])
+    ):
+        return self.mdl.encode(sentences, batch_size=batch_size, **kwargs)
 
-        return np.array(embeddings)
-
-    def encode_queries(
-        self,
-        queries: list[str],
-        batch_size: int = 250,
-        prompt_name: str | None = None,
-        **kwargs: Any,
-    ) -> np.ndarray:
-        return self.encode(
-            queries, batch_size=batch_size, prompt_name=prompt_name, **kwargs,
+    def encode_queries(self, queries: list[str], batch_size: int = 32, **kwargs: Any):
+        if "prompt_name" in kwargs:
+            kwargs.pop("prompt_name")
+        sentences = [
+            "Represent this sentence for searching relevant passages: " + sentence
+            for sentence in queries
+        ]
+        emb = self.mdl.encode(
+            sentences, batch_size=batch_size, normalize_embeddings=True, **kwargs
         )
+        return emb
 
     def encode_corpus(
         self,
         corpus: list[dict[str, str]] | dict[str, list[str]],
-        batch_size: int = 250,
-        prompt_name: str | None = None,
+        batch_size: int = 32,
         **kwargs: Any,
-    ) -> np.ndarray:
+    ):
+        if "prompt_name" in kwargs:
+            kwargs.pop("prompt_name")
         sentences = corpus_to_texts(corpus)
-        return self.encode(
-            sentences, batch_size=batch_size, prompt_name=prompt_name, **kwargs,
+        emb = self.mdl.encode(
+            sentences, batch_size=batch_size, normalize_embeddings=True, **kwargs
         )
+        return emb
 
-bge_m3 = ModelMeta(
-    loader=partial(BGEWrapper, model_name="BAAI/bge-m3"),  # type: ignore
-    name="BAAI/bge-m3",
-    languages=["eng_Latn"],
-    open_source=True,
-    revision="a5beb1e3e68b9ab74eb54cfd186867f64f240e1a",
-    release_date="2023-09-11",  # initial commit of hf model.
-)
 
 bge_base_en_v1_5 = ModelMeta(
     loader=partial(BGEWrapper, model_name="BAAI/bge-base-en-v1.5"),  # type: ignore
